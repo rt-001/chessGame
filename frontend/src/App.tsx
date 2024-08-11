4b8bf98c100be2a3fea3c2ef8ab7007047dbdffd
Romit Tiwari <romittiwari100@gmail.com>
2024-08-11 13:52:45 +0530

    Done

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,21 @@
-
-/* eslint-disable @typescript-eslint/no-unused-vars */
-
 import "./App.css";
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import {Landing} from "./screens/Landing";
+import { Game } from './screens/Game';
 function App() {
   return (
-  <>
-  <BrowserRouter basename="/app">
+  <div className="bg-slate-950 h-screen w-screen flex justify-center">
+
+  
+<BrowserRouter>
     <Routes>
-        <Route path="/" element={}></Route>
+    <Route path="/" element={<Landing />} />
+    <Route path="/game" element={<Game />} />
     </Routes>
   </BrowserRouter>
-  </>
+  </div>
+  
+  
   );
 }
 
